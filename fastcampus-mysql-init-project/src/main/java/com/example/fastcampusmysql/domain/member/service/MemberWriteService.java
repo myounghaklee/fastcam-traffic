85826210607af23f08f040452d550175ef48da49
devhak <devhak.enzo@gmail.com>
2024-10-25 16:38:50 +0900

    db connection setting

---
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusmysql.domain.member.service;
+
+public class MemberWriteService {
+    public void create(){
+        /*
+         목표 : 회원정보 등록
+         - 닉네임 10자 넘으면 안됨
+         파라미터 : memberRegisterCommand
+         val member = Member.fo(memberRegisterCommand)
+         memberRepoistory.save();
+         */
+    }
+}
