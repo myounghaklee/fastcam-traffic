a05dddf5f8e63b9378cd426dfff95015efd8254d
devhak <devhak.enzo@gmail.com>
2024-10-29 00:31:31 +0900

    member조회 쿼리생성

--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
@@ -2,20 +2,55 @@ package com.example.fastcampusmysql.domain.member.repository;
 
 import com.example.fastcampusmysql.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
 public class MemberRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private static final String TABLE = "member";
     public Member save (Member member){
         return member;
     }
 
+    public Optional<Member> findById(Long id){
+        var sql = String.format("SELECT * FROM %s WHRER id = :id", TABLE);
+        var param = new MapSqlParameterSource().addValue("id", id);
+        RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum ) -> Member
+                .builder()
+                .id(resultSet.getLong("id"))
+                .email(resultSet.getString("email"))
+                .nickname(resultSet.getString("nickname"))
+                .birthday(resultSet.getObject("birthday", LocalDate.class))
+                .createAt(resultSet.getObject("createdAt", LocalDateTime.class))
+                .build();
+        var member = namedParameterJdbcTemplate.queryForObject(sql, param, rowMapper);
+        return Optional.ofNullable(member)
+    }
     public Member insert(Member member){
-        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate());
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
+                .withTableName("Member")
+                .usingGeneratedKeyColumns("id");
+        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+        var id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return Member.builder()
+                .id(id)
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .birthday(member.getBirthday())
+                .createAt(member.getCreateAt())
+                .build();
     }
 
     public Member update(Member member){
