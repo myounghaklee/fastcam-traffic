ff5a73cd22af1e98a86cd314e72aaeb7fb2b93e1
devhak <devhak.enzo@gmail.com>
2024-11-29 18:11:14 +0900

    nickname change 오류수정

--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -2,21 +2,35 @@ package com.example.fastcampusmysql.domain.member.service;
 
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.entity.MemberNickNameHistory;
 import com.example.fastcampusmysql.domain.member.repository.MemberNicknameHistoryRepository;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class MemberWriteService {
-    private final MemberNicknameHistoryRepository memberRepository;
+    private final MemberNicknameHistoryRepository memberNicknameHistoryRepository;
+    private final MemberRepository memberRepository;
     public Member create(RegisterMemberCommand command){
         var member = Member.builder()
                 .nickname(command.nickname())
                 .email(command.email())
                 .birthday(command.birthDate())
                 .build();
+        var savedMember = memberRepository.save(member);
+        savedMemberNicknameHistory(savedMember);
+
         return memberRepository.save(member);
+    }
+
+    private void savedMemberNicknameHistory(Member member) {
+        var history = MemberNickNameHistory.builder()
+                .memberId(member.getId())
+                .nickname(member.getNickname())
+                .build();
+        memberNicknameHistoryRepository.save(history);
     }
 
     public void changeNickname(Long memberId, String nickname){
