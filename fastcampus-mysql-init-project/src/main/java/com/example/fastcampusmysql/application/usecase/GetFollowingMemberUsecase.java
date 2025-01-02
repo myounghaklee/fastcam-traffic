ec7801808df59897c6a4a895f9029acb585418f8
devhak <devhak.enzo@gmail.com>
2025-01-02 22:54:15 +0900

    follow 조회 로직 개발
    member 오류 수정

--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/application/usecase/GetFollowingMemberUsecase.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/application/usecase/GetFollowingMemberUsecase.java
@@ -1,16 +1,24 @@
 package com.example.fastcampusmysql.application.usecase;
 
 import com.example.fastcampusmysql.domain.member.dto.MemberDto;
+import com.example.fastcampusmysql.domain.member.service.FollowReadService;
+import com.example.fastcampusmysql.domain.member.service.MemberReadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
+@Service
 public class GetFollowingMemberUsecase {
+    private final MemberReadService memberReadService;
+    private final FollowReadService followReadService;
 
     public List<MemberDto> execute(Long memberId){
         /*
         1. fromMemberId = memberId -> Follow List
         2. 1번을 순회하면서 회원정보 찾기
          */
-
+        var followings = followReadService.getFollowings(memberId);
     }
 }
--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -20,25 +21,40 @@ import java.util.Optional;
 public class MemberRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private static final String TABLE = "Member";
-    public Member save (Member member){
-        return member;
+    private static RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum) -> Member
+            .builder()
+            .id(resultSet.getLong("id"))
+            .email(resultSet.getString("email"))
+            .nickname(resultSet.getString("nickname"))
+            .birthday(resultSet.getObject("birthday", LocalDate.class))
+            .createAt(resultSet.getObject("createdAt", LocalDateTime.class))
+            .build();
+
+    public Member save(Member member) {
+        if(member.getId() == null){
+            return insert(member);
+        }
+        return update(member);
     }
 
-    public Optional<Member> findById(Long id){
+    public Optional<Member> findById(Long id) {
         var sql = String.format("SELECT * FROM %s WHERE id = :id", TABLE);
         var param = new MapSqlParameterSource().addValue("id", id);
-        RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum ) -> Member
-                .builder()
-                .id(resultSet.getLong("id"))
-                .email(resultSet.getString("email"))
-                .nickname(resultSet.getString("nickname"))
-                .birthday(resultSet.getObject("birthday", LocalDate.class))
-                .createAt(resultSet.getObject("createdAt", LocalDateTime.class))
-                .build();
+
+        List<Member> members = namedParameterJdbcTemplate.query(sql, param, rowMapper);
+
         var member = namedParameterJdbcTemplate.queryForObject(sql, param, rowMapper);
         return Optional.ofNullable(member);
     }
-    public Member insert(Member member){
+
+    public List<Member> findAllByIdIn(List<Long> ids) {
+        if (ids.isEmpty()) return List.of();
+        var sql = String.format("SELECT * FROM %s WHERE id in(:ids)", TABLE);
+        var params = new MapSqlParameterSource().addValue("ids", ids);
+        return namedParameterJdbcTemplate.query(sql, params, rowMapper);
+    }
+
+    public Member insert(Member member) {
         SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
                 .withTableName("Member")
                 .usingGeneratedKeyColumns("id");
@@ -53,7 +69,8 @@ public class MemberRepository {
                 .build();
     }
 
-    public Member update(Member member){
+
+    public Member update(Member member) {
         var sql = String.format("UPDATE %s set email = :email, nickname= nickname, birthday = :birthday WHERE id = :id", TABLE);
         SqlParameterSource params = new BeanPropertySqlParameterSource(member);
         namedParameterJdbcTemplate.update(sql, params);
