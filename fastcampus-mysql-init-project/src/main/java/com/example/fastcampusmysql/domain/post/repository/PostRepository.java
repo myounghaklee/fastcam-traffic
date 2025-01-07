d29405be945f49a517f97ed3584b5d27c6285f3e
devhak <devhak.enzo@gmail.com>
2025-01-07 21:21:07 +0900

    post조회 api 생성

--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -1,12 +1,19 @@
 package com.example.fastcampusmysql.domain.post.repository;
 
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.entity.Post;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -14,6 +21,23 @@ public class PostRepository {
 
     static final String TABLE = "Post";
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static final RowMapper<DailyPostCount> DAILY_POST_COUNT_ROW_MAPPER = (ResultSet resultSet, int rowNum) -> new DailyPostCount(
+            resultSet.getLong("memberId"),
+            resultSet.getObject("createdDate", LocalDate.class),
+            resultSet.getLong("count")
+    );
+
+    public List<DailyPostCount> groupByCreatedDate(DailyPostCountRequest request) {
+        var sql = String.format("""
+                SELECT createdDate, memberId, count(id)
+                FROM %s
+                WHERE memberId = :memberId and createdDate between :firstDate and :lastDate
+                GROUP BY memberid, createdDate 
+                """, TABLE);
+        var params = new BeanPropertySqlParameterSource(request);
+        return namedParameterJdbcTemplate.query(sql, params, DAILY_POST_COUNT_ROW_MAPPER);
+    }
 
     public Post save(Post post) {
         if (post.getId() == null) {
---
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
@@ -0,0 +1,21 @@
+package com.example.fastcampusmysql.domain.post.service;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostReadService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public List<DailyPostCount> getDailyPostCount(DailyPostCountRequest request){
+        return postRepository.groupByCreatedDate(request);
+    }
+}
