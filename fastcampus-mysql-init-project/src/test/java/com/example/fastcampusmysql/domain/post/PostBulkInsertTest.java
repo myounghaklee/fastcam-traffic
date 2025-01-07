8f0bc64147143ede8d1e3309b4d47cbad29c9add
devhak <devhak.enzo@gmail.com>
2025-01-07 22:56:45 +0900

    bulk insert 테스트코드

---
+++ fastcampus-mysql-init-project/src/test/java/com/example/fastcampusmysql/domain/post/PostBulkInsertTest.java
@@ -0,0 +1,28 @@
+package com.example.fastcampusmysql.domain.post;
+
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import com.example.fastcampusmysql.util.PostFixtureFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class PostBulkInsertTest {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void bulkInsert(){
+        var easyRandom = PostFixtureFactory.get(3L, LocalDate.of(2022,1,1),
+                LocalDate.of(2022,2,1));
+
+        IntStream.range(0,10)
+                .mapToObj(i -> easyRandom.nextObject(Post.class))
+                .forEach(x ->
+                        System.out.println(x.getMemberId() + "/" + x.getCreatedAt()));
+    }
+}
